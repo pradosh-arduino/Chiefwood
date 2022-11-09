c158c8fb59fd67551f4d2739e4271ed4baee7803
pradosh-arduino <pradoshnathan@gmail.com>
2022-11-09 15:28:45 +0530

    Format Fix 1

--- Program.cs
+++ Program.cs
@@ -75,8 +75,7 @@ namespace pradosh_arduino
 
         public static string DecryptString(string base64EncodedData)
         {
-            var base64EncodedBytes =
-                System.Convert.FromBase64String(base64EncodedData);
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
 
